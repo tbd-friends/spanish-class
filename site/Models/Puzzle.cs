223f8cbe40ef5eaa63b176a1b3886ee04f72fa83
Terry Burns-Dyson <terrybd@gmail.com>
2020-04-07 22:05:31 -0400

    Styling and layout
    - Cleaned up general puzzle design
    - Hid layout, so need urls
    - Changed drag and drop to Spanish to test

--- site/Models/Puzzle.cs
+++ site/Models/Puzzle.cs
@@ -6,6 +6,7 @@ namespace site.Models
     {
         public Guid Id { get; set; }
         public string Type { get; set; }
+        public string Name { get; set; }
         public bool Status { get; set; }
     }
 }
