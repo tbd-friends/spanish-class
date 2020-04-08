d435809a116810fe46b3517bb87eceddac6970a8
Terry Burns-Dyson <terrybd@gmail.com>
2020-04-07 20:22:56 -0400

    Added image provided by Junicus
    Corrected coords in puzzle, added puzzle back to set.

--- site/Infrastructure/StringExtensions.cs
+++ site/Infrastructure/StringExtensions.cs
@@ -4,11 +4,16 @@ using System.Linq;
 
 namespace site.Infrastructure
 {
-    public static class CharacterExtensions
+    public static class StringExtensions
     {
         public static IEnumerable<char> ToCharArray(this IEnumerable<int> values)
         {
             return values.Select(c => Convert.ToChar(c));
+        }
+
+        public static IEnumerable<string> Randomize(this IEnumerable<string> values)
+        {
+            return values.Select(s => (id: Guid.NewGuid(), value: s)).OrderBy(o => o.id).Select(s => s.value);
         }
     }
 }
